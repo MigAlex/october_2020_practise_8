c98013423d998a6e018760bd3de16d54e68d3def
Alex <awangardowie@gmail.com>
2020-10-25 20:02:19 +0100

    routing

--- lib/screens/home_main.dart
+++ lib/screens/home_main.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 
-
 class HomeMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(accountName: null, accountEmail: null),
+            ListTile(
+              title: Text('Dismissible'),
+              trailing: Icon(Icons.gesture),
+              onTap: () => Navigator.pushNamed(context, '/dismissible'),
+            ),
+            ListTile(
+              title: Text('Slidable'),
+              trailing: Icon(Icons.slideshow_sharp),
+              onTap: () => Navigator.pushNamed(context, '/slidable'),
+            ),
+            ListTile(
+              title: Text('To Do List'),
+              trailing: Icon(Icons.list),
+              onTap: () => Navigator.pushNamed(context, '/toDoList'),
+            ),
+          ],
+        ),
+      ),
+      body: Container(),
     );
   }
 }
